476f2d363fb58609d3be827503303f4e0eeaa5f5
Bearbetas <144781445+Bearbetas@users.noreply.github.com>
2024-05-17 10:37:56 +0200

    Skapar Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,51 @@
-import { useEffect } from 'react'
+import Login from './Login'
+import Cart from './Cart'
+import Home from './Home'
+import Faves from './Faves'
 
+import {
+  createHashRouter,
+  Link,
+  Outlet,
+  RouterProvider
+} from 'react-router-dom'
 
 function App() {
+  const router = createHashRouter([
+    {
+      children: [
+        { element: <Home />, path: '/' },
+        { element: <Login />, path: '/login' },
+        { element: <Cart />, path: '/cart' },
+        { element: <Faves />, path: '/Faves' }
+      ],
+      element: (
+        <>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Hem</Link>
+              </li>
+              <li>
+                <Link to="/Login">Login</Link>
+              </li>
+              <li>
+                <Link to="/Faves">Favoriter</Link>
+              </li>
+              <li>
+                <Link to="/Cart">Varukorg</Link>
+              </li>
+            </ul>
+          </nav>
+          <main>
+            <Outlet />
+          </main>
+        </>
+      )
+    }
+  ])
 
-  useEffect(() => {
-    fetch('/api')
-      .then((response) => response.json())
-      .then((result) => {
-        alert(`Hello ${result.hello}!`)
-      })
-  }, [])
-
-
+  return <RouterProvider router={router} />
 }
+
 export default App
