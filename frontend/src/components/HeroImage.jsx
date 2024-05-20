aaccf07f8bc893a313b733de97827f291ba05a49
Bearbetas <144781445+Bearbetas@users.noreply.github.com>
2024-05-20 11:32:25 +0200

    HeroImage

---
+++ frontend/src/components/HeroImage.jsx
@@ -0,0 +1,9 @@
+import Image from 'react-bootstrap/Image'
+
+
+
+function HeroImage() {
+  return <Image src = "/HeroApples.jpg" style={{width: '100%'}}  />
+}
+
+export default HeroImage
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { StyledEngineProvider } from '@mui/material/styles'
-import NavBar from './components/NavBar.jsx'
+
 import App from './App.jsx'
 import './index.css'
+import HeroImage from './components/HeroImage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-    <NavBar />
-    </StyledEngineProvider>
+    <HeroImage />
     <App />
   </React.StrictMode>
 )
