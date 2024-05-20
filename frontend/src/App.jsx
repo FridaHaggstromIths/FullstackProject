2fb7eed8bea616133d20dae225744ed3303e7907
FridaHaggstromIths <frida.haggstrom@iths.se>
2024-05-20 11:31:25 +0200

    ändrat strukturen, fixat app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+//import { Link, Routes, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Login from './components/Login'
 import Cart from './components/Cart'
 import Home from './components/Home'
@@ -5,10 +11,30 @@ import Faves from './components/Faves'
 
 import {
   createHashRouter,
-  Link,
   Outlet,
   RouterProvider
-} from 'react-router-dom'
+} from 'react-router-dom';
+
+function Root() {
+  return (
+    <>
+    <Navbar fixed="top"  bg="light" data-bs-theme="light">
+        <Container>
+        <Navbar.Brand as={Link} to="/">Fruktkorg</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Sortiment</Nav.Link>
+            <Nav.Link as={Link} to="/login">Logga in</Nav.Link>
+            <Nav.Link as={Link} to="/faves">Favoriter</Nav.Link>
+            <Nav.Link as={Link} to="/cart">Varukorg</Nav.Link>
+          </Nav>
+        </Container>
+    </Navbar>
+        <main>
+          <Outlet />
+        </main>
+    </>
+  )
+}
 
 function App() {
   const router = createHashRouter([
@@ -17,31 +43,12 @@ function App() {
         { element: <Home />, path: '/' },
         { element: <Login />, path: '/login' },
         { element: <Cart />, path: '/cart' },
-        { element: <Faves />, path: '/Faves' }
+        { element: <Faves />, path: '/Faves' },
+        { element: <Faves />, path: '/Sort' }
       ],
-      element: (
-        <>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Hem</Link>
-              </li>
-              <li>
-                <Link to="/Login">Login</Link>
-              </li>
-              <li>
-                <Link to="/Faves">Favoriter</Link>
-              </li>
-              <li>
-                <Link to="/Cart">Varukorg</Link>
-              </li>
-            </ul>
-          </nav>
-          <main>
-            <Outlet />
-          </main>
-        </>
-      )
+
+      element: <Root  />
+
     }
   ])
 
